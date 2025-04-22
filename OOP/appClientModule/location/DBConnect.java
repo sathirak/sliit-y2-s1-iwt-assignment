b149cf07a85db2bcadb65d0bfcd7787a3c4d809a
Kavindu Maduhansa Liyanage <kavindumaduhansa78@gmail.com>
2025-04-22 09:39:39 +0530

    feat : update DBConnect.java

--- OOP/appClientModule/location/DBConnect.java
+++ OOP/appClientModule/location/DBConnect.java
@@ -2,21 +2,26 @@ package com.location;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnect {
-    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
-    private static final String USER = "sql2774435";
-    private static final String PASSWORD = "eApaLQH2Sd";
+	
+	private static String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/?user=sql12774435";
+	private static String userName = "sql12774435";
+	private static String password = "eApaLQH2Sd";
+	private static Connection con;
 
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
-    }
+	public static Connection getConnection() {
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager.getConnection(url, userName, password);
+
+		} catch (Exception e) {
+			System.out.println("Database connection is not success!!!");
+		}
+
+		return con;
+	}
 }
