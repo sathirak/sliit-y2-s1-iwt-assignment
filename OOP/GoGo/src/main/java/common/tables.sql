13f2be8043c7950edd17f81ccd24295ea17761b9
Thehara Heshanjali <thehara001@gmail.com>
2025-05-07 21:28:33 +0530

    feat: add Reservation CRUD

---
+++ OOP/GoGo/src/main/java/common/tables.sql
@@ -0,0 +1,5 @@
+use sql5776488;
+
+INSERT INTO User (first_name, last_name, license_no, license_expiry_date, email, password, contact_no)
+VALUES 
+('Jane', 'Smith', 'DrL543a21', '2025-05-15', 'janeith@example.com', 'janepassass', '9876543210');
