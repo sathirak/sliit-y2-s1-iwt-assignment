2adddbd0431dc75e211dee4b1f21c69cac87e342
Kavindu Maduhansa Liyanage <kavindumaduhansa78@gmail.com>
2025-05-07 10:50:53 +0530

    feat : updated updateLocation.java

--- OOP/src/location/UpdateLocation.java
+++ OOP/src/location/UpdateLocation.java
@@ -4,37 +4,48 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 @WebServlet("/location/update")
 public class UpdateLocation extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public UpdateLocation() {
+        super();
+    }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String locationIdStr = request.getParameter("location_id");
+
+        if (locationIdStr != null) {
+            Location location = LocationDBUtil.getLocationDetails(locationIdStr);
+
+            if (location != null) {
+                request.setAttribute("location", location);
+                request.getRequestDispatcher("/updateLocation.jsp").forward(request, response);
+            } else {
+                response.getWriter().write("Location not found.");
+            }
+        } else {
+            response.getWriter().write("Invalid location ID.");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String locationId = request.getParameter("location_id");
         String district = request.getParameter("district");
         String locationContactNo = request.getParameter("location_contact_no");
         String streetNo = request.getParameter("street_no");
         String street = request.getParameter("street");
         String city = request.getParameter("city");
-        
 
-        boolean isTrue;
-        isTrue = LocationDBUtil.updateLocation(locationId, district, locationContactNo, streetNo, street, city);
+        boolean isTrue = LocationDBUtil.updateLocation(locationId, district, locationContactNo, streetNo, street, city);
 
-        if (isTrue == true) {
-        	
-            RequestDispatcher dis = request.getRequestDispatcher("success.jsp");
-            dis.forward(request, response);
-            
+        if (isTrue) {
+            response.sendRedirect(request.getContextPath() + "/location?location_id=" + locationId);
         } else {
-        	
-            RequestDispatcher dis2 = request.getRequestDispatcher("unsuccess.jsp");
-            dis2.forward(request, response);
+            RequestDispatcher dis = request.getRequestDispatcher("unsuccess.jsp");
+            dis.forward(request, response);
         }
     }
 }
