a46847a188e0b24f73cf488c0c79f55eb14c27c9
Thilakshi Weliwaththa <130579853+thilu17@users.noreply.github.com>
2025-05-10 12:50:35 +0530

    feat:about us servlet

---
+++ aboutus.java
@@ -0,0 +1,22 @@
+package common;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/aboutus")
+public class aboutus extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	  public aboutus() {
+	        super();
+	    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/aboutus.jsp").forward(request, response);
+	}
+
+}
